5418f4c05adc9bb51a78e78bff8d3ba2322a87a6
prusov-code <67950687+prusov-code@users.noreply.github.com>
2021-06-15 18:23:43 +0300

    Add files via upload

--- js/language_change.js
+++ js/language_change.js
@@ -6,6 +6,14 @@ $('.english_language').on('click',function () {
 	    $('.footer_regular[href="https://vk.com/nik_prusov"]').slideUp();
 	    $('.contacts>h1').slideUp();
 	    $('.contacts>h3').slideUp();
+	     for(var i=1;i<4;i++) {
+		     $('section>h1').eq(i).slideUp();
+	     }
+	     for(var i=0;i<4;i++) {
+		     $('#technical_part>h3').eq(i).slideUp();
+	     }
+ 	     
+ 	     $('.summary>a>p').slideUp();
 		setTimeout(function() {
 	  		$('.nav').html('<a href="#about_me"><li>About me</li></a><a href="#principles"><li>Work principles</li></a><a href="#works"><li>Works</li></a><a href="#technical_part"><li>Technical section</li></a><a href="#contacts"><li>Contacts</li></a>');
 	  		$('.nav').slideDown();
@@ -19,6 +27,23 @@ $('.english_language').on('click',function () {
 		    $('.contacts>h1').slideDown();
 		    $('.contacts>h3').text('Interested in my services? Contact me using the contacts below');
 		    $('.contacts>h3').slideDown();
+		     $('section>h1').eq(1).text('About me');
+		     $('section>h1').eq(2).text('Work principles');
+		     $('section>h1').eq(3).text('My works');
+ 		     $('section>h1').eq(4).text('Technical information');
+     	     for(var i=1;i<4;i++) {
+			     $('section>h1').eq(i).slideDown();
+		     }
+	         $('#technical_part>h3').eq(0).text('Main stack');
+		     $('#technical_part>h3').eq(1).text('Frameworks,libraries');
+		     $('#technical_part>h3').eq(2).text('Databases');
+ 		     $('#technical_part>h3').eq(3).text('Additional knowledge');
+ 		     $('#technical_part>h3').eq(4).text('Summary:');
+     	     for(var i=0;i<4;i++) {
+			     $('#technical_part>h3').eq(i).slideDown();
+		     }
+		       $('.summary>a>p').text('Download summary');
+		       $('.summary>a>p').slideDown();
 	 	}, 1000);	
 	}
 });
@@ -30,6 +55,13 @@ $('.russian_language').on('click',function () {
 	     $('.footer_regular[href="https://vk.com/nik_prusov"]').slideUp();
 	        $('.contacts>h1').slideUp();
 	        $('.contacts>h3').slideUp();
+	          for(var i=1;i<4;i++) {
+		     $('section>h1').eq(i).slideUp();
+	     }
+	     for(var i=0;i<4;i++) {
+		     $('#technical_part>h3').eq(i).slideUp();
+	     }
+ 	        $('.summary>a>p').slideUp();
 		setTimeout(function() {
 	  		$('.nav').html('<a href="#about_me"><li>Обо мне</li></a><a href="#principles"><li>Принципы работы</li></a><a href="#works"><li>Работы</li></a><a href="#technical_part"><li>Технический раздел</li></a><a href="#contacts"><li>Контакты</li></a>');
 	  		$('.nav').slideDown();
@@ -43,6 +75,23 @@ $('.russian_language').on('click',function () {
 		    $('.contacts>h1').slideDown();
 		    $('.contacts>h3').text('Интересуют услуги? Свяжитесь со мной при помощи контактов ниже');
 		    $('.contacts>h3').slideDown();
+		     $('section>h1').eq(1).text('Обо мне');
+		     $('section>h1').eq(2).text('Принципы работы');
+		     $('section>h1').eq(3).text('Работы');
+ 		     $('section>h1').eq(4).text('Технический раздел');
+     	     for(var i=1;i<4;i++) {
+			     $('section>h1').eq(i).slideDown();
+		     }
+	         $('#technical_part>h3').eq(0).text('Основной стек:');
+		     $('#technical_part>h3').eq(1).text('Фреймворки, библиотеки и другие технологии:');
+		     $('#technical_part>h3').eq(2).text('Базы данных:');
+ 		     $('#technical_part>h3').eq(3).text('Дополнительные знания:');
+ 		     $('#technical_part>h3').eq(4).text('Резюме:');
+     	     for(var i=0;i<4;i++) {
+			     $('#technical_part>h3').eq(i).slideDown();
+		     }
+		            $('.summary>a>p').text('Скачать резюме');
+		       $('.summary>a>p').slideDown();
 	 	}, 1000);	
 	}
 });
