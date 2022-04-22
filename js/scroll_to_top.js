104c2800637c0cbd7a878d8cd13e64531c8b1a4a
Piga <67950687+prusov-code@users.noreply.github.com>
2022-04-22 23:19:32 +0300

    Update scroll_to_top.js

--- js/scroll_to_top.js
+++ js/scroll_to_top.js
@@ -1,13 +1,16 @@
 $(function(){
 	$(window).scroll(function(){
 		if($(window).scrollTop() > 100) {
+			$('#scroll_top').css('transition','inherit')
 			$('#scroll_top').fadeIn();
 		} else {
+			$('#scroll_top').css('transition','inherit')
 			$('#scroll_top').fadeOut();
 		}
 	});
  
 	$('#scroll_top').click(function(){
+		$(this).css('transition','inherit')
 		$("html, body").animate({scrollTop: 0});
 		return false;
 	});
