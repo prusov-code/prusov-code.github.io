6730255bed7bffcc03fab900e9788799ae7ca51b
Prusov-code <67950687+prusov-code@users.noreply.github.com>
2022-11-04 22:36:07 +0300

    Add files via upload

--- js/popup_website_info.js
+++ js/popup_website_info.js
@@ -8,6 +8,10 @@ $('.popup_exit_button').on('click', function() {
 	$('body').css('overflow-y','visible');
 	$(this).parent().slideToggle('slow');
 });
+$('.popup_exit').on('click', function() {
+	$('body').css('overflow-y','visible');
+	$(this).parent().slideToggle('slow');
+});
 $('.leibmedic').on('click', function() {
   //$('.website_info_popup_leibmedic>.inner').html('<iframe width="100%" height="80vh" src="https://leibmedic.ru/" name="iframe" scrolling="auto"></iframe>');
 });
